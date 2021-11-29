2cf4a33e71714e14e67f6170c9400fe3b4c26c12
ash-ley <ashleyritchie1871@hotmail.co.uk>
2021-11-29 18:54:28 +0000

    UPDATE - fixed the bash script

--- script.sh
+++ script.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+PATH=/usr/local/bin:/usr/bin:/usr/local/sbin:/usr/sbin:/home/ec2-user/.local/bin:/home/ec2-user/bin
 FILE=/tmp/file
 KEY=/home/ec2-user/.ssh/id_rsa
 
@@ -16,8 +17,11 @@ if test -f "\$FILE"; then
     # Find the next server to send it to
     	MYIP=\$(ifconfig eth0 | awk '\$1 == "inet" {print \$2}')
     	IP=\$(nmap --exclude \$MYIP -Pn -p22 --open \$MYIP-254 -oG - | awk '/Up\$/{print \$2}' | head -n 1)
+		echo "Next server: \$IP"
 	if [ -z "\${IP}" ]; then
+		echo "No servers found, checking from the start"
 		IP=\$(nmap --exclude \$MYIP -Pn -p22 --open 192.168.25.4-254 -oG - | awk '/Up\$/{print \$2}' | head -n 1)
+		echo "Next server: \$IP"
 	fi
     # send the file
     scp -o StrictHostKeyChecking=no -i \$KEY \$FILE ec2-user@\$IP:\$FILE
